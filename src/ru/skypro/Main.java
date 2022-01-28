8e7d8dbd298c80860fc85e920573e55728893ca7
Namig1991 <safarovnamig09@gmail.com>
2022-01-28 16:13:57 +0300

    Home - work "Переменные1" Учел все поправки от Olga Lugacheva!

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,89 +6,178 @@ public class Main {
 	//
     var box = 5;
     System.out.println(box);
+
     byte lesson = 8;
+
     short course = 9;
+
     int profession = 10;
+
     long year = 11L;
+
     float day = 1.1221f;
+
     double car = 1.1231231;
+
+    char cat = 60000;
+
+    boolean b = 10 > 9;
+
+    boolean c = 66 < 3;
+
     var boxer1 = 78.2;
+
     var boxer2 = 82.7;
-    var totalWeight = boxer1 + boxer2;
+
+    short totalWeight = (short) ( boxer1 + boxer2) ;
+
     System.out.println("Общий вес боксеров " +totalWeight + "кг");
-    var differenceWeight = boxer2 - boxer1;
+
+    double differenceWeight = boxer2 - boxer1;
+
     System.out.println("Разница в весе " + differenceWeight + " кг");
-    var bananas = 5;
-    var oneBananas = 80;
-    var totalBananas = bananas * oneBananas;
+
+    byte bananas = 5;
+
+    byte oneBananas = 80;
+
+    short totalBananas = (short) (bananas * oneBananas);
+
     System.out.println("Общий вес бананов " + totalBananas + " грамм");
-    var milk = 200;
-    var milk100ml = 105;
-    var totalMilk = milk100ml * 2;
+
+    short milk = 200;
+
+    byte milk100ml = 105;
+
+    short totalMilk = (short) (milk100ml * 2);
+
     System.out.println("Всего молока " + totalMilk + " грамм");
-    var iceCrem = 2;
-    var oneIceCrem = 100;
-    var totalIceCrem = iceCrem * oneIceCrem;
+
+    byte iceCrem = 2;
+
+    byte oneIceCrem = 100;
+
+    short totalIceCrem = (short) (iceCrem * oneIceCrem);
+
     System.out.println("Общий вес пломбира " + totalIceCrem + " грамм");
-    var aggs = 4;
-    var oneAgg = 70;
-    var totalAggs = aggs * oneAgg;
+
+    byte aggs = 4;
+
+    byte oneAgg = 70;
+
+    short totalAggs = (short) (aggs * oneAgg);
+
     System.out.println("Общий вес яиц " + totalAggs + " грамм");
-    var cocktail = totalBananas + totalMilk + totalIceCrem + totalAggs;
+
+    short cocktail = (short) (totalBananas + totalMilk + totalIceCrem + totalAggs);
+
     System.out.println("Общий вес спорт-завтрака " + cocktail + " грамм");
+
     float cocktailGm = 1090;
-    float coctailKg = cocktailGm / 1000;
-    System.out.println("В одной порции завтрака " + coctailKg + " кг!");
-    var excessWeight = 7;
+
+    float cocktailKg = cocktailGm / 1000;
+
+    System.out.println("В одной порции завтрака " + cocktailKg + " кг!");
+
+    int excessWeight = 7;
+
     System.out.println("Лишний вес " + excessWeight + " кг");
+
     excessWeight = excessWeight * 1000;
+
     System.out.println("Лишний вес " + excessWeight + "грамм");
-    var firstMethod = 250;
+
+    short firstMethod = 250;
+
     excessWeight = excessWeight / firstMethod;
+
     System.out.println("Если терять по 250 грамм каждый день, понадобится " + excessWeight + " Дней!");
-    var secondMethod = 500;
-        var excessWeight1 = 7;
+
+    short secondMethod = 500;
+
+        short excessWeight1 = 7;
+
         System.out.println("Лишний вес " + excessWeight1 + " кг");
-        excessWeight1 = excessWeight1 * 1000;
+
+        excessWeight1 = (short) (excessWeight1 * 1000);
+
         System.out.println("Лишний вес " + excessWeight1 + "грамм");
-        excessWeight1 = excessWeight1 / secondMethod;
+
+        excessWeight1 = (short) (excessWeight1 / secondMethod);
+
         System.out.println("Если терять по 500 грамм каждый день, понадобится " + excessWeight1 + " Дней!");
-        var onAverageDays = (excessWeight + excessWeight1) / 2;
+
+        short onAverageDays = (short) ((excessWeight + excessWeight1) / 2);
+
         System.out.println("В среднем для похудения понадобиться " + onAverageDays + " днь!");
-        var masha1 = 67760;
-        var masha2 = (masha1 * 10) / 100;
+
+        int masha1 = 67760;
+
+        short masha2 = (short) ((masha1 * 10) / 100);
+
         System.out.println( "10% от нынешней зарплаты Маши " + masha2 + " RUB");
-        var masha3 = masha1 + masha2;
+
+        int masha3 = masha1 + masha2;
+
         System.out.println("Маша теперь получает в Месяц " + masha3 + " RUB");
-        var masha4 = masha1 * 12;
+
+        int masha4 = masha1 * 12;
+
         System.out.println("Годовой доход Маши до повышения " + masha4 + " RUB");
-        var masha5 = masha3 * 12;
+
+        int masha5 = masha3 * 12;
+
         System.out.println(" Годовой доход Маши после повышения з/п на 10% " + masha5 + " RUB");
-        var masha6 = masha5 - masha4;
+
+        int masha6 = masha5 - masha4;
+
         System.out.println(" Годовой доход Маши вырос на " + masha6 + " RUB");
-        var denis1 = 83690;
-        var denis2 = (denis1 * 10) / 100;
+
+        int denis1 = 83690;
+
+        short denis2 = (short) ((denis1 * 10) / 100);
+
         System.out.println( "10% от нынешней зарплаты Дениса " + denis2 + " RUB");
-        var denis3 = denis1 + denis2;
+
+        int denis3 = denis1 + denis2;
+
         System.out.println("Денис теперь получает в Месяц " + denis3 + " RUB");
-        var denis4 = denis1 * 12;
+
+        int denis4 = denis1 * 12;
+
         System.out.println("Годовой доход Дениса до повышения " + denis4 + " RUB");
-        var denis5 = denis3 * 12;
+
+        int denis5 = denis3 * 12;
+
         System.out.println(" Годовой доход Дениса после повышения з/п на 10% " + denis5 + " RUB");
-        var denis6 = denis5 - denis4;
+
+        int denis6 = denis5 - denis4;
+
         System.out.println(" Годовой доход Дениса вырос на " + denis6 + " RUB");
-        var kris1 = 76230;
-        var kris2 = (kris1 * 10) / 100;
+
+        int kris1 = 76230;
+
+        short kris2 = (short) ((kris1 * 10) / 100);
+
         System.out.println( "10% от нынешней зарплаты Кристины " + kris2 + " RUB");
-        var kris3 = kris1 + kris2;
+
+        int kris3 = kris1 + kris2;
+
         System.out.println("Кристина теперь получает в Месяц " + kris3 + " RUB");
-        var kris4 = kris1 * 12;
+
+        int kris4 = kris1 * 12;
+
         System.out.println("Годовой доход Кристины до повышения " + kris4 + " RUB");
-        var kris5 = kris3 * 12;
+
+        int kris5 = kris3 * 12;
+
         System.out.println(" Годовой доход Кристины после повышения з/п на 10% " + kris5 + " RUB");
-        var kris6 = kris5 - kris4;
+
+        int kris6 = kris5 - kris4;
+
         System.out.println(" Годовой доход Кристины вырос на " + kris6 + " RUB");
-        System.out.println("Hello, World!");
+
+
 
 
 
